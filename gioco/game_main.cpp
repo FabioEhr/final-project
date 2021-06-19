0d92707792734e99ee6d281bbb78a383e2e73e8b
Stef Gaudenzio Lootens <stef.lootens@studio.unibo.it>
2021-06-19 18:04:57 +0200

    added main

---
+++ gioco/game_main.cpp
@@ -0,0 +1,26 @@
+#include "decisions.hpp"
+#include "events.hpp"
+#include <string> 
+
+int main() {
+    
+    std::cout << "basic welcome message + some commands and rules"
+    City active_city;
+
+    while (true) {
+        // selection of city
+        break;
+    } 
+    
+    while (true) {
+        
+        std::string u_input;
+        std::cin >> u_input;
+
+        if (u_input == "lockdown") {
+            std::cout << "you selected lockdown \n";
+        };
+
+        break;
+    }
+}
