ea7a7578048c8b88ae1f66b5bc168d26492a310e
Lorenzo Mensi <lorenzo.mensi@studio.unibo.it>
2021-06-26 18:43:33 +0200

    Fixed endless input bug, added tests. Program is ready to go

--- sir_base/sir.cpp
+++ sir_base/sir.cpp
@@ -1,13 +1,15 @@
 #include "sir.hpp"
+#include "useful_func.hpp"
 int main() {
   try {
     sir::pandemy play = sir::createVirus();
 
     int N = 10;
-
+    std::string a;
     std::cout << "quanti giorni vuoi far evolvere l'epidemia?";
 
-    std::cin >> N;
+    std::cin >> a;
+    N=string_to_int(a);
 
     std::vector<sir::condition> decorso = play.evolveNTimes(N);
 
--- sir_base/sir.test.cpp
+++ sir_base/sir.test.cpp
@@ -13,82 +13,113 @@ TEST_CASE("Testing stable lives") {
 
   sir::condition bad{0.5, 0.1, 0.4, 0};
 
-  sir::pandemy bau{dummy, bad};
+  sir::pandemy b{dummy, bad};
 
   CHECK(bad.suscettibles == 0.5);
 
-  bau.evolve();
+  b.evolve();
 
-  CHECK(bau.situationP.time == 1);
+  CHECK(b.situationP.time == 1);
 
-  CHECK(bau.situationP.suscettibles == 0.5);
+  CHECK(b.situationP.suscettibles == 0.5);
 
-  CHECK(bau.situationP.infected == 0.);
+  CHECK(b.situationP.infected == 0.);
 
-  CHECK(bau.situationP.recovered == 0.5);
-
-}
-
-TEST_CASE("Testing edge case 1"){
-
-    try{
-
-sir::virus dummy{-1.,0.4};
-
-sir::condition bad{0.5, 0.1, 0.4, 0};
-
-sir::pandemy bau{dummy, bad};
+  CHECK(b.situationP.recovered == 0.5);
 
 }
 
-catch(std::runtime_error const& e) {
 
-
-CHECK(e.what()==std::string{ "Contagiousnees must be a positive value"});
-
+TEST_CASE("Testing different contagiousness"){
+    sir::virus higher= {0.6,0.};
+    sir::virus lower={0.3,0.};
+    sir::condition same= {0.99,0.01,0.,0};
+    sir::pandemy test_1= {lower, same};
+    sir::pandemy test_2= {higher, same};
+    test_1.evolveNTimes(10);
+    test_2.evolveNTimes(10);
+    CHECK(test_1.situationP.infected < test_2.situationP.infected);
+    CHECK(test_1.situationP.suscettibles > test_2.situationP.suscettibles);
+    CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
 }
 
+TEST_CASE("Testing recovery rates"){
+    sir::virus higher= {0.,0.6};
+    sir::virus lower={0.,0.2};
+    sir::condition same= {0.,1.,0.,0};
+    sir::pandemy test_1= {lower, same};
+    sir::pandemy test_2= {higher, same};
+    test_1.evolveNTimes(10);
+    test_2.evolveNTimes(10);
+    CHECK(test_1.situationP.infected > test_2.situationP.infected);
+    CHECK(test_1.situationP.recovered < test_2.situationP.recovered);
+    CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
 }
+TEST_CASE("Testing that percentages always add up to 1"){
+    sir::virus alpha_var= {0.2, 0.01};
+    sir::virus beta_var= {0.4, 0.05};
+    sir::virus gamma_var= {0., 1.};
+    sir::virus delta_var= {1., 0.};
+    sir::condition same= {1., 0., 0., 0};
+    sir::pandemy test_1= {alpha_var,same};
+    sir::pandemy test_2= {beta_var,same};
+    sir::pandemy test_3= {gamma_var,same};
+    sir::pandemy test_4= {delta_var, same};
+    test_1.evolveNTimes(5);
+test_2.evolveNTimes(5);
+test_3.evolveNTimes(5);
+test_4.evolveNTimes(5);
+CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_3.situationP.infected+test_3.situationP.suscettibles+test_3.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_4.situationP.infected+test_4.situationP.suscettibles+test_4.situationP.recovered, 0.99, 1.01) );
+test_1.evolveNTimes(5);
+test_2.evolveNTimes(5);
+test_3.evolveNTimes(5);
+test_4.evolveNTimes(5);
+CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_3.situationP.infected+test_3.situationP.suscettibles+test_3.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_4.situationP.infected+test_4.situationP.suscettibles+test_4.situationP.recovered, 0.99, 1.01) );
+test_1.evolveNTimes(5);
+test_2.evolveNTimes(5);
+test_3.evolveNTimes(5);
+test_4.evolveNTimes(5);
+CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_3.situationP.infected+test_3.situationP.suscettibles+test_3.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_4.situationP.infected+test_4.situationP.suscettibles+test_4.situationP.recovered, 0.99, 1.01) );
+test_1.evolveNTimes(5);
+test_2.evolveNTimes(5);
+test_3.evolveNTimes(5);
+test_4.evolveNTimes(5);
+CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_3.situationP.infected+test_3.situationP.suscettibles+test_3.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_4.situationP.infected+test_4.situationP.suscettibles+test_4.situationP.recovered, 0.99, 1.01) );
+test_1.evolveNTimes(5);
+test_2.evolveNTimes(5);
+test_3.evolveNTimes(5);
+test_4.evolveNTimes(5);
+CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_3.situationP.infected+test_3.situationP.suscettibles+test_3.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_4.situationP.infected+test_4.situationP.suscettibles+test_4.situationP.recovered, 0.99, 1.01) );
+test_1.evolveNTimes(5);
+test_2.evolveNTimes(5);
+test_3.evolveNTimes(5);
+test_4.evolveNTimes(5);
+CHECK(sir::good(test_1.situationP.infected+test_1.situationP.suscettibles+test_1.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_2.situationP.infected+test_2.situationP.suscettibles+test_2.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_3.situationP.infected+test_3.situationP.suscettibles+test_3.situationP.recovered, 0.99, 1.01) );
+ CHECK(sir::good(test_4.situationP.infected+test_4.situationP.suscettibles+test_4.situationP.recovered, 0.99, 1.01) );
 
+}
  
 
-TEST_CASE("Testing edge case 2"){
 
-    try{
 
-sir::virus dummy{0.3,4.};
 
-sir::condition bad{0.5, 0.1, 0.4, 0};
-
-sir::pandemy bau{dummy, bad};
-
-}
-
-catch(std::runtime_error const& e) {
-
-CHECK(e.what()== std::string{"Recovery rate must be between 0 and 1 " });
-
-}
-
-}
-
-TEST_CASE("Testing edge case 3"){
-
-    try{
-
-sir::virus dummy{0.3,.2};
-
-sir::condition bad{2.5, 0.1, 0.4, 0};
-
-sir::pandemy bau{dummy, bad};
-
-}
-
-catch(std::runtime_error const& e) {
-
-CHECK(e.what()== std::string{" The percentage of infected, susceptible and recovered people must add up to 1" });
-
-}
-
-}
 
